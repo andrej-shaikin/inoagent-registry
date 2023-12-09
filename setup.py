4f34b9b6ce6ec57e02b60133a1022d2ed6d82bdf
kiwibon <kiwibon@yandex.ru>
2023-12-09 21:51:02 +0300

    корректируем ссылку на репозиторий

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='inoagent-registry',
-    version='1.0.0',
+    version='1.0.1',
     packages=[
         'inoagent'
     ],
     python_requires='>=3.10',
-    url='https://gitlab.com/kiwib/inoagent-control',
+    url='https://github.com/andrej-shaikin/inoagent-registry',
     license='MIT',
     author='Andrey Shaikin',
     author_email='kiwibon@yandex.ru',
